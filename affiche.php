640bd8440a7fc233bed89148acb54ce2698af2a6
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-02 21:03:56 +0100

    products

---
+++ affiche.php
@@ -0,0 +1,43 @@
+<?php include("./models/crud_produit.php");
+$pr = new Produit();
+
+$liste = $pr->list_produits();
+?>
+<div class="col-sm-9 padding-right">
+    <div class="features_items">
+        <!--features_items-->
+        <h2 class="title text-center">Articles récents</h2>
+        <?php
+        while ($row = $liste->fetch_assoc()) {
+        ?>
+        <div class="col-sm-4">
+            <div class="product-image-wrapper">
+                <div class="single-products">
+                    <div class="productinfo text-center">
+                        <img src="<?= $row["photo"] ?>" alt="" />
+                        <h2><?= $row["prix"] ?></h2>
+                        <p><?= $row["description"] ?></p>
+                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Ajouter au
+                            panier</a>
+                    </div>
+                    <div class="product-overlay">
+                        <div class="overlay-content">
+                            <h2><?= $row["prix"] ?></h2>
+                            <p><?= $row["description"] ?></p>
+                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Ajouter
+                                au panier</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <?php } ?>
+
+
+    </div>
+    <!--features_items-->
+
+
+
+</div>
--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!--/head-->
 <?php
-include('./imports/imports.php')
+include('./imports/imports.php');
+//
+//include('./models/crud_produit.php');
 
+?>
+<?php
+//$pr = new Produit();
+
+//$liste = $pr->list_produits();
 ?>
 
 <body>
     <?php
-    include('./imports/header.php')
+    include('./imports/header.php');
 
     ?>
     <!--/header-->
@@ -75,35 +82,15 @@ include('./imports/imports.php')
                 <div class="col-sm-9 padding-right">
                     <div class="features_items">
                         <!--features_items-->
-                        <h2 class="title text-center">Articles récents</h2>
-                        <div class="col-sm-4">
-                            <div class="product-image-wrapper">
-                                <div class="single-products">
-                                    <div class="productinfo text-center">
-                                        <img src="images/home/product1.jpg" alt="" />
-                                        <h2>56TND</h2>
-                                        <p>Easy Polo Black Edition</p>
-                                        <a href="#" class="btn btn-default add-to-cart"><i
-                                                class="fa fa-shopping-cart"></i>Ajouter au panier</a>
-                                    </div>
-                                    <div class="product-overlay">
-                                        <div class="overlay-content">
-                                            <h2>$56</h2>
-                                            <p>Easy Polo Black Edition</p>
-                                            <a href="#" class="btn btn-default add-to-cart"><i
-                                                    class="fa fa-shopping-cart"></i>Ajouter au panier</a>
-                                        </div>
-                                    </div>
-                                </div>
 
-                            </div>
-                        </div>
 
 
                     </div>
                     <!--features_items-->
 
-
+                    <?php
+                    include("affiche.php");
+                    ?>
 
                 </div>
             </div>
