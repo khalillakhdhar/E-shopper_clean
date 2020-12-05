be4ac759543ba3b73785a1930f72c8d77f9b0755
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-05 07:25:31 +0100

    Update crud_commande.php

--- models/crud_commande.php
+++ models/crud_commande.php
@@ -84,7 +84,7 @@ class Commande
             die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql = "SELECT photo,quantite,date,etat,user.email,user.id,user.nom,user.telephone,user.adresse,produit.prix,produit.description,produit.categorie,produit.id_user,produit.titre FROM `commande` ,produit ,user WHERE produit.id=commande.id_produit AND produit.id_user='" . $_SESSION['id'] . "'";
+        $sql = "SELECT photo,quantite,date,etat,user.email,user.id,user.nom,user.telephone,user.adresse,produit.prix,produit.description,produit.categorie,produit.id_user,produit.titre FROM `commande` ,produit ,user WHERE produit.id=commande.id_produit AND commande.id_user='" . $_SESSION['id'] . "'";
         $result = $conn->query($sql);
         return $result;
     }
